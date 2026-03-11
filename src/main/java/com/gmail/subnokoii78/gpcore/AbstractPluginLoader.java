9ff50beff984a4c278af12be0d6747aa2d60f13a
たけのこII <yu2.aoki0229.@gmail.com>
2026-03-11 18:11:20 +0900

    a

--- src/main/java/com/gmail/subnokoii78/gpcore/AbstractPluginLoader.java
+++ src/main/java/com/gmail/subnokoii78/gpcore/AbstractPluginLoader.java
@@ -2,30 +2,51 @@ package com.gmail.subnokoii78.gpcore;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
+import io.papermc.paper.plugin.loader.library.impl.JarLibrary;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 
+import java.nio.file.Path;
 import java.util.Set;
 
-public abstract class AbstractGPLoader implements PluginLoader {
-    public abstract Set<String> getMavenDependencies();
+public abstract class AbstractPluginLoader implements PluginLoader {
+    public abstract Set<String> getDependencies();
+
+    public abstract Set<Path> getJarLibraries();
 
     @Override
     public final void classloader(PluginClasspathBuilder classpathBuilder) {
         final MavenLibraryResolver resolver = new MavenLibraryResolver();
+
         resolver.addRepository(new RemoteRepository.Builder(
             "central",
             "default",
             MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR
         ).build());
 
-        for (final String library : getMavenDependencies()) {
+        resolver.addRepository(new RemoteRepository.Builder(
+            "paper",
+            "default",
+            "https://repo.papermc.io/repository/maven-public/"
+        ).build());
+
+        resolver.addRepository(new RemoteRepository.Builder(
+            "minecraft",
+            "default",
+            "https://libraries.minecraft.net/"
+        ).build());
+
+        for (final String library : getDependencies()) {
             resolver.addDependency(new Dependency(
                 new DefaultArtifact(library),
                 null
             ));
+        }
+
+        for (final Path path : getJarLibraries()) {
+            classpathBuilder.addLibrary(new JarLibrary(path));
         }
 
         classpathBuilder.addLibrary(resolver);
